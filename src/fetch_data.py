69e23269b204db2dd426390d1ac3f9dbbf551076
Chinda Ada <chindaada63@gmail.com>
2026-05-04 22:39:43 +0100

    update

--- src/fetch_data.py
+++ src/fetch_data.py
@@ -1,1 +1,7 @@
 """Fetching data from yfinance"""
+import logging
+
+import pandas as pd
+import yfinance as yf
+
+"""I will use US stock data because Nigerian data APIs have less historical depth than US """
