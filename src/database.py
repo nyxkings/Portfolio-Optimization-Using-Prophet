3298df28d32f36fb6f0c44a864b8437e5cb18852
Chinda Ada <chindaada63@gmail.com>
2026-05-04 21:11:09 +0100

    another update

--- src/database.py
+++ src/database.py
@@ -3,10 +3,3 @@
 import yfinance as yf
 
 
-import yfinance as yf
-import pandas as pd
-
-# Download some test data
-data = yf.download("AAPL", period="5d")
-print("\n✓ Successfully downloaded Apple stock data:")
-print(data.head())
